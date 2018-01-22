098d1d512e4d892a08dcc319e7aa22ee8ec48971
baka_.laptop <baka_@laptop>
2018-01-22 22:40:27 +0300

    edit home work

--- src/components/CommentsForm.js
+++ src/components/CommentsForm.js
@@ -9,15 +9,19 @@ export default class CommentsFrom extends Component {
     }
 
     changeUser = (ev) => {
-        let useValid = (ev.target.value.length > 5 && ev.target.value.length < 15) ? true : false
+        if (ev.target.value.length < 15) return
+
+        let userValid = (ev.target.value.length > 5) ? true : false
         this.setState({
             user: ev.target.value,
-            useValid: useValid
+            userValid: userValid
         })
     }
 
     changeComment = (ev) => {
-        let textValid = (ev.target.value.length > 20 && ev.target.value.length < 50) ? true : false
+        if (ev.target.value.length < 50) return
+
+        let textValid = (ev.target.value.length > 20) ? true : false
         this.setState({
             text: ev.target.value,
             textValid: textValid
@@ -26,23 +30,15 @@ export default class CommentsFrom extends Component {
 
 
     render() {
-        const userValid = this.state.userValid ? 'green' : 'red'
-        const textValid = this.state.textValid ? 'green' : 'red'
-
-        console.log('------------')
-        console.log('userValid', this.state.user.length)
-        console.log('userValid', userValid)
-        console.log('------------')
-        console.log('textValid', this.state.text.length)
-        console.log('textValid', textValid)
-        console.log('------------')
+        const userValid = this.state.userValid ? '' : 'red'
+        const textValid = this.state.textValid ? '' : 'red'
 
         return (
             <div>
                 <p>Enter User Name</p>
-                <input style={{border: `2px solid ${userValid}`}} type="text" value={this.state.user} onChange={this.changeUser} /> <br />
+                <input style={{border: `1px solid ${userValid}`}} type="text" value={this.state.user} onChange={this.changeUser} /> <br />
                 <p>Enter Comment</p>
-                <textarea style={{border: `2px solid ${textValid}`}} value={this.state.text} onChange={this.changeComment} />
+                <textarea style={{border: `1px solid ${textValid}`}} value={this.state.text} onChange={this.changeComment} />
             </div>
         )
     }
