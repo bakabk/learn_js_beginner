9091cce19660d94c352ffaa15af9f9b389527908
Yan.C Work <baka_@bk.ru>
2018-01-22 15:51:53 +0300

    add UserForm + ex validation

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react'
 import ArticleList from './ArticleList'
 import ArticleCharts from './ArticleCharts'
+import UserForm from './UserForm'
 
 class App extends Component{
     render (){
         return(
             <div>
+                <UserForm />
                 <ArticleList articles={this.props.articles} />
                 <ArticleCharts articles={this.props.articles} />
             </div>
