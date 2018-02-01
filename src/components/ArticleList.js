37867ac86fff4a1cf16e2c46165bc2ec778b00ca
Yan.C Work <baka_@bk.ru>
2018-02-01 18:48:30 +0300

    add reselect

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import Article from './Article'
 import accordion from '../decorators/accordion'
 import {connect} from 'react-redux'
+import {filteredArticlesSelector} from '../selectors'
 
 class ArticleList extends Component{
     static propTypes = {
@@ -32,17 +33,8 @@ class ArticleList extends Component{
     }
 }
 
-export default connect(
-    ({filters, articles}) => {
-    const {selected, dateRange: {from, to}} = filters
-
-    const filteredArticles =  articles.filter(article => {
-        const published = Date.parse(article.date)
-        return (!selected.length || selected.includes(article.id)) &&
-            (!from || !to || (published > from && published < to))
-    })
-
+export default connect((state) => {
     return {
-        articles: filteredArticles
+        articles: filteredArticlesSelector(state)
     }
 })(accordion(ArticleList))
