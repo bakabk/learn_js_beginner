2fad1b4bf804ee72322b7c9a025526ea9cbbd07d
baka_.laptop <baka_@laptop>
2018-02-07 01:28:44 +0300

    normalize comments

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import {connect} from 'react-redux'
 
-export default function Comment({comment}) {
+function Comment({comment}) {
     return(
         <div>
             <p>{`Commented by ${comment.user}`}</p>
@@ -11,8 +12,15 @@ export default function Comment({comment}) {
 }
 
 Comment.protoType = {
+    id: PropTypes.string.isRequired,
+    //from connect
     comment: PropTypes.shape({
         text: PropTypes.string.isRequired,
         user: PropTypes.string.isRequired
     }).isRequired
 }
+
+export default connect((state, ownProps) => {
+    return {comment: state.comments.find(comment => comment.id === ownProps.id)}
+
+})(Comment)
--- src/components/Comments.js
+++ src/components/Comments.js
@@ -21,7 +21,7 @@ function getComments({comments, isOpen}) {
     return (
         <div>
             <ul>
-                {comments.map(comment => <li key={comment.id}><Comment comment={comment}/></li>)}
+                {comments.map(id => <li key={id}><Comment id={id}/></li>)}
             </ul>
             <CommentsForm/>
         </div>
